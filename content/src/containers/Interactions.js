bff46051a0d640d319a1c4ba121eac200c1abd5d
Abby Cloft <acloft@uci.edu>
2018-03-13 15:45:44 -0700

    formated docs

--- content/src/containers/Interactions.js
+++ content/src/containers/Interactions.js
@@ -1,97 +1,106 @@
-import React from 'react'
-import InteractionForm from './InteractionForm'
-import * as interactionService from '../services/interaction.service'
+import React from "react";
+import InteractionForm from "./InteractionForm";
+import * as interactionService from "../services/interaction.service";
 
 class Interactions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      interactions: []
+    };
+    this.onSelect = this.onSelect.bind(this);
+    this.onCancel = this.onCancel.bind(this);
+    this.onSave = this.onSave.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+  }
+  componentDidMount() {
+    interactionService
+      .readAll()
+      .then(data => {
+        //console.log(data)
+        this.setState({ interactions: data.data.items });
+      })
+      .catch(err => console.log(err));
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            interactions: []
-        }
-        this.onSelect = this.onSelect.bind(this)
-        this.onCancel = this.onCancel.bind(this)
-        this.onSave = this.onSave.bind(this)
-        this.onDelete = this.onDelete.bind(this)
-    }
-    componentDidMount() {
-        interactionService.readAll()
-            .then(data => {
-                //console.log(data)
-                this.setState({ interactions: data.data.items })
-            })
-            .catch(err => console.log(err))
-    }
+  onSelect(item, event) {
+    event.preventDefault();
+    this.setState({
+      formData: item
+    });
+  }
 
-    onSelect(item, event) {
-        event.preventDefault()
-        this.setState({
-            formData: item
-        })
-    }
+  onCancel() {
+    this.setState({ formData: null });
+  }
 
-    onCancel() {
-        this.setState({ formData: null })
-    }
-
-    onDelete() {
-        debugger
-        // let id = formData._id.value
-        const formData = this.state.formData;
-        interactionService.deleteById(formData._id)
-            .then(() => {
-                this.setState(prevState => {
-                    const updatedItems = prevState.interactions.filter(item => item._id !== formData._id)
-                    return { interactions: updatedItems }
-                })
-                this.onCancel()
-            })
-            .catch(err => console.log(err))
-    }
-
-    onSave(updatedFormData) {
+  onDelete() {
+    debugger;
+    // let id = formData._id.value
+    const formData = this.state.formData;
+    interactionService
+      .deleteById(formData._id)
+      .then(() => {
         this.setState(prevState => {
-            const existingItem = prevState.interactions.filter(item => {
-                return item._id === updatedFormData._id;
-            })
-            let updatedItems = [];
-            if (existingItem && existingItem.length > 0) {
-                updatedItems = prevState.interactions.map(item => {
-                    return item._id === updatedFormData._id ? updatedFormData : item
-                })
-            }
-            else {
-                updatedItems = prevState.interactions.concat(updatedFormData);
-            }
-            return {
-                interactions: updatedItems,
-                formData: null,
-                errorMessage: null
-            }
-        })
-    }
+          const updatedItems = prevState.interactions.filter(
+            item => item._id !== formData._id
+          );
+          return { interactions: updatedItems };
+        });
+        this.onCancel();
+      })
+      .catch(err => console.log(err));
+  }
 
+  onSave(updatedFormData) {
+    this.setState(prevState => {
+      const existingItem = prevState.interactions.filter(item => {
+        return item._id === updatedFormData._id;
+      });
+      let updatedItems = [];
+      if (existingItem && existingItem.length > 0) {
+        updatedItems = prevState.interactions.map(item => {
+          return item._id === updatedFormData._id ? updatedFormData : item;
+        });
+      } else {
+        updatedItems = prevState.interactions.concat(updatedFormData);
+      }
+      return {
+        interactions: updatedItems,
+        formData: null,
+        errorMessage: null
+      };
+    });
+  }
 
-    render() {
-        const interactions = this.state.interactions ? this.state.interactions.map(interaction => (
-            <li key={interaction._id} onClick={this.onSelect.bind(this, interaction)}>{`ID: ${interaction._id}`}</li>
-        )) : <h2> NONE</h2>
+  render() {
+    const interactions = this.state.interactions ? (
+      this.state.interactions.map(interaction => (
+        <li
+          key={interaction._id}
+          onClick={this.onSelect.bind(this, interaction)}
+        >{`ID: ${interaction._id}`}</li>
+      ))
+    ) : (
+      <h2> NONE</h2>
+    );
 
-        return (<div>
-            <h1> Interactions</h1>
-            {interactions}
+    return (
+      <div>
+        <h1> Interactions</h1>
+        {interactions}
 
-            <div>
-                <InteractionForm
-                    formData={this.state.formData}
-                    onSave={this.onSave}
-                    onDelete={this.onDelete}
-                    onCancel={this.onCancel}
-                />
-
-            </div>
-        </div>)
-    }
+        <div>
+          <InteractionForm
+            formData={this.state.formData}
+            onSave={this.onSave}
+            onDelete={this.onDelete}
+            onCancel={this.onCancel}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Interactions
+export default Interactions;
--- content/src/services/interaction.service.js
+++ content/src/services/interaction.service.js
@@ -1,44 +1,41 @@
-import axios from 'axios'
+import axios from "axios";
 
-const headers = {}
-const baseUrl = `http://localhost:8080/api/interactions`
+const headers = {};
+const baseUrl = `http://localhost:8080/api/interactions`;
 
+export function create(data) {
+  const config = {
+    method: "POST",
+    headers,
+    data: data
+  };
 
-
-export function create (data){
-    const config = {
-        method: 'POST', 
-        headers,
-        data: data
-    }
-
-    return axios(baseUrl, config)
+  return axios(baseUrl, config);
 }
 
-export function readAll(){
-    const config = {
-        method: 'GET', 
-        headers
-    }
-    return axios(baseUrl, config)
-
+export function readAll() {
+  const config = {
+    method: "GET",
+    headers
+  };
+  return axios(baseUrl, config);
 }
 
-export function update(data){
-    const config ={
-        method: 'PUT',
-        headers,
-        data: data
-    }
+export function update(data) {
+  const config = {
+    method: "PUT",
+    headers,
+    data: data
+  };
 
-   return axios(`${baseUrl}/${data._id}`, config)
+  return axios(`${baseUrl}/${data._id}`, config);
 }
 
-export function deleteById(id){
-    const config = {
-        method: 'DELETE', 
-        headers, 
-    }
+export function deleteById(id) {
+  const config = {
+    method: "DELETE",
+    headers
+  };
 
-    return axios(`${baseUrl}/${id}`, config)
+  return axios(`${baseUrl}/${id}`, config);
 }
