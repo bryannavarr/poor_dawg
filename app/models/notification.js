754495e17617942f683ebe6e0adb176141e0185a
Casey Spinner <csspin@gmail.com>
2018-03-13 17:01:41 -0700

    made message required

--- app/models/notification.js
+++ app/models/notification.js
@@ -6,7 +6,7 @@ const schema = {
     dogOwnerId: Joi.objectId().required(),
     _id: Joi.objectId(),
     dogId: Joi.objectId().required(),
-    message: Joi.string(),
+    message: Joi.string().required(),
     type: Joi.string().valid(['Challenges', 'Wellness']),
     createDate: Joi.date().iso().default(() => new Date, 'date of creation'),
     updateDate: Joi.date().iso().default(() => new Date, 'date of update')
