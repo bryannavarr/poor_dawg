12ce6d12452f79c175cde7a607227f471c0258d6
Abby Cloft <acloft@uci.edu>
2018-03-14 10:18:21 -0700

    formatting

--- app/controllers/notifications.controller.js
+++ app/controllers/notifications.controller.js
@@ -1,78 +1,82 @@
-const responses = require('../models/responses');
-const notificationsService = require('../services/notifications.service')
-const apiPrefix = '/api/notifications';
+const responses = require("../models/responses");
+const notificationsService = require("../services/notifications.service");
+const apiPrefix = "/api/notifications";
 
 module.exports = {
-    readAll: readAll,
-    readById: readById,
-    create: create,
-    update: update,
-    delete: _delete
-}
+  readAll: readAll,
+  readById: readById,
+  create: create,
+  update: update,
+  delete: _delete
+};
 
 function readAll(req, res) {
-    notificationsService.readAll()
-        .then(notifications => {
-            const responseModel = new responses.ItemsResponse()
-            responseModel.items = notifications
-            res.json(responseModel)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).send(new responses.ErrorResponse(err))
-        })
+  notificationsService
+    .readAll()
+    .then(notifications => {
+      const responseModel = new responses.ItemsResponse();
+      responseModel.items = notifications;
+      res.json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(new responses.ErrorResponse(err));
+    });
 }
 
 function readById(req, res) {
-    notificationsService.readById(req.params.id)
-        .then(notification => {
-            const responseModel = new responses.ItemsResponse()
-            responseModel.item = notification
-            res.json(responseModel)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).send(new responses.ErrorResponse(err))
-        })
+  notificationsService
+    .readById(req.params.id)
+    .then(notification => {
+      const responseModel = new responses.ItemsResponse();
+      responseModel.item = notification;
+      res.json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(new responses.ErrorResponse(err));
+    });
 }
 
 function create(req, res) {
-    notificationsService.create(req.model)
-        .then(id => {
-            const responseModel = new responses.ItemResponse()
-            responseModel.item = id
-            res.status(201)
-                .location(`${apiPrefix}/${id}`)
-                .json(responseModel)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).send(new responses.ErrorResponse(err))
-        })
+  notificationsService
+    .create(req.model)
+    .then(id => {
+      const responseModel = new responses.ItemResponse();
+      responseModel.item = id;
+      res
+        .status(201)
+        .location(`${apiPrefix}/${id}`)
+        .json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(new responses.ErrorResponse(err));
+    });
 }
 
 function update(req, res) {
-    notificationsService
-        .update(req.params.id, req.model)
-        .then(notification => {
-            const responseModel = new responses.SuccessResponse()
-            res.status(200).json(responseModel)
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).send(new responses.ErrorResponse(err))
-        })
+  notificationsService
+    .update(req.params.id, req.model)
+    .then(notification => {
+      const responseModel = new responses.SuccessResponse();
+      res.status(200).json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send(new responses.ErrorResponse(err));
+    });
 }
 
 function _delete(req, res) {
-    notificationsService
-        .delete(req.params.id)
-        .then(() => {
-            const responseModel = new responses.SuccessResponse()
-            res.status(200).json(responseModel)
-        })
-        .catch(err => {
-            console.log(err)
-            return res.status(500).send(new responses.ErrorResponse(err))
-        })
+  notificationsService
+    .delete(req.params.id)
+    .then(() => {
+      const responseModel = new responses.SuccessResponse();
+      res.status(200).json(responseModel);
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).send(new responses.ErrorResponse(err));
+    });
 }
