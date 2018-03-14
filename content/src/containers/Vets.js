7bd840d6c3f8265d126d53652f67516f81847f64
John Darragh <darragh@entrotech.net>
2018-03-13 17:11:33 -0700

    Start styling vets

--- content/src/containers/Vets.js
+++ content/src/containers/Vets.js
@@ -1,103 +1,107 @@
-import React from 'react'
-import * as vetService from '../services/vet.service'
-import VetForm from './VetForm'
+import React from "react";
+import * as vetService from "../services/vet.service";
+import VetForm from "./VetForm";
 
 class Vets extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            vets: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      vets: []
+    };
 
-        this.onCancel = this.onCancel.bind(this)
-        this.onDelete = this.onDelete.bind(this)
-        this.onSave = this.onSave.bind(this)
-        this.onSelect = this.onSelect.bind(this)
-    }
+    this.onCancel = this.onCancel.bind(this);
+    this.onDelete = this.onDelete.bind(this);
+    this.onSave = this.onSave.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+  }
 
-    componentDidMount() {
-        vetService.readAll().then(data => {
-            this.setState({ vets: data.items })
-        })
-    }
+  componentDidMount() {
+    vetService.readAll().then(data => {
+      this.setState({ vets: data.items });
+    });
+  }
 
-    onCancel() {
-        this.setState({ formData: null })
-    }
+  onCancel() {
+    this.setState({ formData: null });
+  }
 
-    onDelete() {
-        const formData = this.state.formData
+  onDelete() {
+    const formData = this.state.formData;
 
-        vetService.del(formData._id)
-            .then(() => {
-                this.setState(prevState => {
-                    const updatedItems = prevState.vets.filter(item => {
-                        return item._id !== formData._id
-                    })
-
-                    return { vets: updatedItems }
-                })
-
-                this.onCancel()
-            })
-            .catch(err => console.log(err))
-    }
-
-    onSave(updatedFormData) {
+    vetService
+      .del(formData._id)
+      .then(() => {
         this.setState(prevState => {
-            const existingItem = prevState.vets.filter(item => {
-                return item._id === updatedFormData._id
-            })
-            let updatedItems = [];
-            if (existingItem && existingItem.length > 0) {
-                updatedItems = prevState.vets.map(item => {
-                    return (
-                        item._id === updatedFormData._id
-                            ? updatedFormData
-                            : item
-                    )
-                })
-            } else {
-                updatedItems = prevState.vets.concat(updatedFormData);
-            }
-            return {
-                vets: updatedItems,
-                formData: null,
-                errorMessage: null
-            }
-        })
-    }
+          const updatedItems = prevState.vets.filter(item => {
+            return item._id !== formData._id;
+          });
 
-    onSelect(item, event) {
-        event.preventDefault()
-        this.setState({
-            formData: item
-        })
-    }
+          return { vets: updatedItems };
+        });
 
-    render() {
-        const vets = this.state.vets ? this.state.vets.map(vet => (
-            <li key={vet._id} onClick={this.onSelect.bind(this, vet)}>{vet.firstName + ' ' + vet.lastName}</li>
-        ))
-            : <React.Fragment></React.Fragment>
+        this.onCancel();
+      })
+      .catch(err => console.log(err));
+  }
 
-        return (
-            <React.Fragment>
-                <ul>
-                    {vets}
-                </ul>
+  onSave(updatedFormData) {
+    this.setState(prevState => {
+      const existingItem = prevState.vets.filter(item => {
+        return item._id === updatedFormData._id;
+      });
+      let updatedItems = [];
+      if (existingItem && existingItem.length > 0) {
+        updatedItems = prevState.vets.map(item => {
+          return item._id === updatedFormData._id ? updatedFormData : item;
+        });
+      } else {
+        updatedItems = prevState.vets.concat(updatedFormData);
+      }
+      return {
+        vets: updatedItems,
+        formData: null,
+        errorMessage: null
+      };
+    });
+  }
 
-                <div>
-                    <VetForm
-                        formData={this.state.formData}
-                        onSave={this.onSave}
-                        onDelete={this.onDelete}
-                        onCancel={this.onCancel}
-                    />
-                </div>
-            </React.Fragment>
-        )
-    }
+  onSelect(item, event) {
+    event.preventDefault();
+    this.setState({
+      formData: item
+    });
+  }
+
+  render() {
+    const vets = this.state.vets ? (
+      this.state.vets.map(vet => (
+        <li key={vet._id} onClick={this.onSelect.bind(this, vet)}>
+          {vet.firstName + " " + vet.lastName}
+        </li>
+      ))
+    ) : (
+      <React.Fragment />
+    );
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className='panel'>
+            <ul>{vets}</ul>
+          </div>
+          <div>
+            <VetForm
+              formData={this.state.formData}
+              onSave={this.onSave}
+              onDelete={this.onDelete}
+              onCancel={this.onCancel}
+            />
+          </div>
+        </div>
+      </div>
+
+    )
+  }
 }
 
-export default Vets
+export default Vets;
