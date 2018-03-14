e7d5d93253bfcadc02a0e3e636e6c392609909a5
Abby Cloft <acloft@uci.edu>
2018-03-14 09:28:47 -0700

    timestamp changed to just live on put methods

--- app/filters/timestamp.js
+++ app/filters/timestamp.js
@@ -1,15 +1,19 @@
+// module.exports = timestamp;
+// function timestamp(req, res, next){
+//     if(req.method === 'PUT'){
+//         req.model.updateDate = new Date();
+//         delete req.model.createDate
+//         next()
+//     }
+//     else{
+//         res.status(500).send('Timestamp does not support this method')
+//     }
+// }
 module.exports = timestamp;
-function timestamp(req, res,next){
-    if(req.method === 'PUT'){
+function timestamp(req, res, next){
         req.model.updateDate = new Date();
         delete req.model.createDate
         next()
-    }
-    else if(req.method === 'POST'){
-        delete req.model.updateDate
-        next()
-    }
-    else{
-        res.status(500).send('Timestamp does not support this method')
-    }
 }
+
+
