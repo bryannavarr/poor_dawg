20bc59f2fadb5b82cd36e66a944f1c45479a93a5
Abby Cloft <acloft@uci.edu>
2018-03-14 10:13:58 -0700

    added in timestamp middleware to put

--- app/routes/notifications.routes.js
+++ app/routes/notifications.routes.js
@@ -2,11 +2,12 @@ const router = require('express').Router()
 const notificationsController = require('../controllers/notifications.controller')
 const validateBody = require('../filters/validate.body')
 const Notification = require('../models/notification')
+const updatedDateTimestamp = require("../filters/timestamp");
 
 module.exports = router
 
 router.get('/', notificationsController.readAll)
 router.get('/:id([0-9a-fA-F]{24})', notificationsController.readById)
 router.post('/', validateBody(Notification), notificationsController.create)
-router.put('/:id([0-9a-fA-F]{24})', validateBody(Notification), notificationsController.update)
+router.put('/:id([0-9a-fA-F]{24})', validateBody(Notification), updatedDateTimestamp, notificationsController.update)
 router.delete('/:id([0-9a-fA-F]{24})', notificationsController.delete)
