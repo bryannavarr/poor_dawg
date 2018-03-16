a62f32a0a34a973c95cafc924e594b36cb655240
Abby Cloft <acloft@uci.edu>
2018-03-16 13:50:55 -0700

    set up for widget grid

---
+++ content/src/containers/widgets/WidgetGrid.js
@@ -0,0 +1,25 @@
+import React from "react";
+import defaults from "./WidgetDefaults";
+import jquery from "jquery";
+window.$ = window.jQuery = jquery;
+require("smartadmin-plugins/smartwidgets/jarvis.widget.ng2.js");
+
+class WidgetGrid extends React.Component {
+  componentDidMount() {
+    window.$(this.grid).jarvisWidgets(defaults);
+  }
+
+  render() {
+    return (
+      <section id="widget-grid" ref={grid => (this.grid = grid)}>
+        {/* {this.props.children} */}
+
+        <div>
+            
+        </div>
+      </section>
+    );
+  }
+}
+
+export default WidgetGrid;
