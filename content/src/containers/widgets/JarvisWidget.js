fbcaa8bd1bbde5d0a16b509a28043c4dbbc11d92
Abby Cloft <acloft@uci.edu>
2018-03-16 13:52:40 -0700

    formatting

--- content/src/containers/widgets/JarvisWidget.js
+++ content/src/containers/widgets/JarvisWidget.js
@@ -1,27 +1,17 @@
 import React from "react";
 
-import InteractionForm from '../InteractionForm'
-
 class JarvisWidget extends React.Component {
-
-
   render() {
     return (
-      <div
-      className= {this.props.classes ? this.props.classes : "jarviswidget"}
-      >
+      <div className={this.props.classes ? this.props.classes : "jarviswidget"}>
         <header>
           <h2>{this.props.title}</h2>
         </header>
 
         <div>
-
-            {this.props.legend ? <legend>{this.props.legend}</legend> : null }
-
+          {this.props.legend ? <legend>{this.props.legend}</legend> : null}
           
-          <div className="widget-body">
-              {this.props.body}
-          </div>
+          <div className="widget-body">{this.props.body}</div>
         </div>
       </div>
     );
