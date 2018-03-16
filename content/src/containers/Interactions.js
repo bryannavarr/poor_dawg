b6c188e025efc6fe537350475327f041bd7522db
Abby Cloft <acloft@uci.edu>
2018-03-15 22:30:51 -0700

    project before importing jquery but after i added smart admin plug ins to node modules in content folder

--- content/src/containers/Interactions.js
+++ content/src/containers/Interactions.js
@@ -1,75 +1,15 @@
+
+import $ from 'jquery'
+
+import 'smartadmin-plugins/smartwidgets/jarvis.widget.ng2.js'
+
 import React from "react";
 import InteractionForm from "./InteractionForm";
 import * as interactionService from "../services/interaction.service";
-import "smartadmin-plugins/smartwidgets/jarvis.widget.ng2.js";
-import "./jarvis.widget.min.js";
-// import $ from "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js";
 
-const defaults = {
-  grid: "article",
-  widgets: ".jarviswidget",
-  localStorage: true,
-  deleteSettingsKey: "#deletesettingskey-options",
-  settingsKeyLabel: "Reset settings?",
-  deletePositionKey: "#deletepositionkey-options",
-  positionKeyLabel: "Reset position?",
-  sortable: true,
-  buttonsHidden: false,
-  // toggle button
-  toggleButton: true,
-  toggleClass: "fa fa-minus | fa fa-plus",
-  toggleSpeed: 200,
-  onToggle: function() {},
-  // delete btn
-  deleteButton: true,
-  deleteMsg: "Warning: This action cannot be undone!",
-  deleteClass: "fa fa-times",
-  deleteSpeed: 200,
-  onDelete: function() {},
-  // edit btn
-  editButton: true,
-  editPlaceholder: ".jarviswidget-editbox",
-  editClass: "fa fa-cog | fa fa-save",
-  editSpeed: 200,
-  onEdit: function() {},
-  // color button
-  colorButton: true,
-  // full screen
-  fullscreenButton: true,
-  fullscreenClass: "fa fa-expand | fa fa-compress",
-  fullscreenDiff: 3,
-  onFullscreen: function() {},
-  // custom btn
-  customButton: false,
-  customClass: "folder-10 | next-10",
-  customStart: function() {
-    alert("Hello you, this is a custom button...");
-  },
-  customEnd: function() {
-    alert("bye, till next time...");
-  },
-  // order
-  buttonOrder: "%refresh% %custom% %edit% %toggle% %fullscreen% %delete%",
-  opacity: 1.0,
-  dragHandle: "> header",
-  placeholderClass: "jarviswidget-placeholder",
-  indicator: true,
-  indicatorTime: 600,
-  ajax: true,
-  timestampPlaceholder: ".jarviswidget-timestamp",
-  timestampFormat: "Last update: %m%/%d%/%y% %h%:%i%:%s%",
-  refreshButton: true,
-  refreshButtonClass: "fa fa-refresh",
-  labelError: "Sorry but there was a error:",
-  labelUpdated: "Last Update:",
-  labelRefresh: "Refresh",
-  labelDelete: "Delete widget:",
-  afterLoad: function() {},
-  rtl: false, // best not to toggle this!
-  onChange: function() {},
-  onSave: function() {},
-  // ajaxnav: $.navAsAjax // declears how the localstorage should be saved (HTML or AJAX Version)
-};
+
+
+
 class Interactions extends React.Component {
   constructor(props) {
     super(props);
@@ -89,8 +29,91 @@ class Interactions extends React.Component {
         this.setState({ interactions: data.data.items });
       })
       .catch(err => console.log(err));
-
-    $(this.grid).jarvisWidgets(defaults);
+      function setup_widgets_desktop() {
+	
+        if ($.fn.jarvisWidgets && true) {
+      
+          $('#widget-grid').jarvisWidgets({
+      
+            grid : 'article',
+            widgets : '.jarviswidget',
+            localStorage : false,
+            deleteSettingsKey : '#deletesettingskey-options',
+            settingsKeyLabel : 'Reset settings?',
+            deletePositionKey : '#deletepositionkey-options',
+            positionKeyLabel : 'Reset position?',
+            sortable : true,
+            buttonsHidden : false,
+            // toggle button
+            toggleButton : true,
+            toggleClass : 'fa fa-minus | fa fa-plus',
+            toggleSpeed : 200,
+            onToggle : function() {
+            },
+            // delete btn
+            deleteButton : true,
+            deleteMsg:'Warning: This action cannot be undone!',
+            deleteClass : 'fa fa-times',
+            deleteSpeed : 200,
+            onDelete : function() {
+            },
+            // edit btn
+            editButton : true,
+            editPlaceholder : '.jarviswidget-editbox',
+            editClass : 'fa fa-cog | fa fa-save',
+            editSpeed : 200,
+            onEdit : function() {
+            },
+            // color button
+            colorButton : true,
+            // full screen
+            fullscreenButton : true,
+            fullscreenClass : 'fa fa-expand | fa fa-compress',
+            fullscreenDiff : 3,
+            onFullscreen : function() {
+            },
+            // custom btn
+            customButton : false,
+            customClass : 'folder-10 | next-10',
+            customStart : function() {
+              alert('Hello you, this is a custom button...');
+            },
+            customEnd : function() {
+              alert('bye, till next time...');
+            },
+            // order
+            buttonOrder : '%refresh% %custom% %edit% %toggle% %fullscreen% %delete%',
+            opacity : 1.0,
+            dragHandle : '> header',
+            placeholderClass : 'jarviswidget-placeholder',
+            indicator : true,
+            indicatorTime : 600,
+            ajax : true,
+            timestampPlaceholder : '.jarviswidget-timestamp',
+            timestampFormat : 'Last update: %m%/%d%/%y% %h%:%i%:%s%',
+            refreshButton : true,
+            refreshButtonClass : 'fa fa-refresh',
+            labelError : 'Sorry but there was a error:',
+            labelUpdated : 'Last Update:',
+            labelRefresh : 'Refresh',
+            labelDelete : 'Delete widget:',
+            afterLoad : function() {
+            },
+            rtl : false, // best not to toggle this!
+            onChange : function() {
+              
+            },
+            onSave : function() {
+              
+            },
+            // ajaxnav : $.navAsAjax // declears how the localstorage should be saved (HTML or AJAX Version)
+      
+          });
+      
+        }
+      
+      }
+      setup_widgets_desktop()
   }
 
   onSelect(item, event) {
