d4506f7a19f5788d4266e4b6e4f44c77f74b34d1
John Lee <john.lee@sabio.la>
2018-03-16 12:01:54 -0700

    john's hacking

--- content/src/containers/Interactions.js
+++ content/src/containers/Interactions.js
@@ -1,14 +1,9 @@
-
-import $ from 'jquery'
-
-import 'smartadmin-plugins/smartwidgets/jarvis.widget.ng2.js'
-
 import React from "react";
 import InteractionForm from "./InteractionForm";
 import * as interactionService from "../services/interaction.service";
-
-
-
+import jquery from "jquery";
+window.$ = window.jQuery = jquery;
+require("smartadmin-plugins/smartwidgets/jarvis.widget.ng2.js");
 
 class Interactions extends React.Component {
   constructor(props) {
@@ -29,91 +24,72 @@ class Interactions extends React.Component {
         this.setState({ interactions: data.data.items });
       })
       .catch(err => console.log(err));
-      function setup_widgets_desktop() {
-	
-        if ($.fn.jarvisWidgets && true) {
-      
-          $('#widget-grid').jarvisWidgets({
-      
-            grid : 'article',
-            widgets : '.jarviswidget',
-            localStorage : false,
-            deleteSettingsKey : '#deletesettingskey-options',
-            settingsKeyLabel : 'Reset settings?',
-            deletePositionKey : '#deletepositionkey-options',
-            positionKeyLabel : 'Reset position?',
-            sortable : true,
-            buttonsHidden : false,
-            // toggle button
-            toggleButton : true,
-            toggleClass : 'fa fa-minus | fa fa-plus',
-            toggleSpeed : 200,
-            onToggle : function() {
-            },
-            // delete btn
-            deleteButton : true,
-            deleteMsg:'Warning: This action cannot be undone!',
-            deleteClass : 'fa fa-times',
-            deleteSpeed : 200,
-            onDelete : function() {
-            },
-            // edit btn
-            editButton : true,
-            editPlaceholder : '.jarviswidget-editbox',
-            editClass : 'fa fa-cog | fa fa-save',
-            editSpeed : 200,
-            onEdit : function() {
-            },
-            // color button
-            colorButton : true,
-            // full screen
-            fullscreenButton : true,
-            fullscreenClass : 'fa fa-expand | fa fa-compress',
-            fullscreenDiff : 3,
-            onFullscreen : function() {
-            },
-            // custom btn
-            customButton : false,
-            customClass : 'folder-10 | next-10',
-            customStart : function() {
-              alert('Hello you, this is a custom button...');
-            },
-            customEnd : function() {
-              alert('bye, till next time...');
-            },
-            // order
-            buttonOrder : '%refresh% %custom% %edit% %toggle% %fullscreen% %delete%',
-            opacity : 1.0,
-            dragHandle : '> header',
-            placeholderClass : 'jarviswidget-placeholder',
-            indicator : true,
-            indicatorTime : 600,
-            ajax : true,
-            timestampPlaceholder : '.jarviswidget-timestamp',
-            timestampFormat : 'Last update: %m%/%d%/%y% %h%:%i%:%s%',
-            refreshButton : true,
-            refreshButtonClass : 'fa fa-refresh',
-            labelError : 'Sorry but there was a error:',
-            labelUpdated : 'Last Update:',
-            labelRefresh : 'Refresh',
-            labelDelete : 'Delete widget:',
-            afterLoad : function() {
-            },
-            rtl : false, // best not to toggle this!
-            onChange : function() {
-              
-            },
-            onSave : function() {
-              
-            },
-            // ajaxnav : $.navAsAjax // declears how the localstorage should be saved (HTML or AJAX Version)
-      
-          });
-      
-        }
-      
-      }
-      setup_widgets_desktop()
+
+    window.$(this.grid).jarvisWidgets({
+      grid: "article",
+      widgets: ".jarviswidget",
+      localStorage: false,
+      deleteSettingsKey: "#deletesettingskey-options",
+      settingsKeyLabel: "Reset settings?",
+      deletePositionKey: "#deletepositionkey-options",
+      positionKeyLabel: "Reset position?",
+      sortable: true,
+      buttonsHidden: false,
+      // toggle button
+      toggleButton: true,
+      toggleClass: "fa fa-minus | fa fa-plus",
+      toggleSpeed: 200,
+      onToggle: function() {},
+      // delete btn
+      deleteButton: true,
+      deleteMsg: "Warning: This action cannot be undone!",
+      deleteClass: "fa fa-times",
+      deleteSpeed: 200,
+      onDelete: function() {},
+      // edit btn
+      editButton: true,
+      editPlaceholder: ".jarviswidget-editbox",
+      editClass: "fa fa-cog | fa fa-save",
+      editSpeed: 200,
+      onEdit: function() {},
+      // color button
+      colorButton: true,
+      // full screen
+      fullscreenButton: true,
+      fullscreenClass: "fa fa-expand | fa fa-compress",
+      fullscreenDiff: 3,
+      onFullscreen: function() {},
+      // custom btn
+      customButton: false,
+      customClass: "folder-10 | next-10",
+      customStart: function() {
+        alert("Hello you, this is a custom button...");
+      },
+      customEnd: function() {
+        alert("bye, till next time...");
+      },
+      // order
+      buttonOrder: "%refresh% %custom% %edit% %toggle% %fullscreen% %delete%",
+      opacity: 1.0,
+      dragHandle: "> header",
+      placeholderClass: "jarviswidget-placeholder",
+      indicator: true,
+      indicatorTime: 600,
+      ajax: true,
+      timestampPlaceholder: ".jarviswidget-timestamp",
+      timestampFormat: "Last update: %m%/%d%/%y% %h%:%i%:%s%",
+      refreshButton: true,
+      refreshButtonClass: "fa fa-refresh",
+      labelError: "Sorry but there was a error:",
+      labelUpdated: "Last Update:",
+      labelRefresh: "Refresh",
+      labelDelete: "Delete widget:",
+      afterLoad: function() {},
+      rtl: false, // best not to toggle this!
+      onChange: function() {},
+      onSave: function() {}
+      // ajaxnav : $.navAsAjax // declears how the localstorage should be saved (HTML or AJAX Version)
+    });
   }
 
   onSelect(item, event) {
@@ -169,12 +145,10 @@ class Interactions extends React.Component {
   render() {
     const interactions = this.state.interactions ? (
       this.state.interactions.map(interaction => (
-        
         <li
           key={interaction._id}
           onClick={this.onSelect.bind(this, interaction)}
         >{`ID: ${interaction._id}`}</li>
-        
       ))
     ) : (
       <h2> NONE</h2>
@@ -191,7 +165,7 @@ class Interactions extends React.Component {
               data-title="refresh"
               rel="tooltip"
               data-placement="bottom"
-              data-original-title="<i class='text-warning fa fa-warning'></i> Warning! This will reset all your widget settings."
+              data-original-title="<i className='text-warning fa fa-warning'></i> Warning! This will reset all your widget settings."
               data-html="true"
             >
               <i className="fa fa-refresh" />
@@ -204,17 +178,17 @@ class Interactions extends React.Component {
           </ol>
         </div>
         <div id="content">
-          <div class="row">
-            <div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
-              <h1 class="page-title txt-color-blueDark">
-                <i class="fa fa-paw fa-fw " />
+          <div className="row">
+            <div className="col-xs-12 col-sm-7 col-md-7 col-lg-4">
+              <h1 className="page-title txt-color-blueDark">
+                <i className="fa fa-paw fa-fw " />
                 Interactions
                 <span>> Add or Update Interactions</span>
               </h1>
             </div>
           </div>
 
-          <section id="widget-grid" ref={(grid) => this.grid = grid}>
+          <section id="widget-grid" ref={grid => (this.grid = grid)}>
             <div className="row">
               <div className="col-sm-6">
                 <ul>{interactions}</ul>
@@ -225,8 +199,7 @@ class Interactions extends React.Component {
                   data-widget-togglebutton="true"
                   id="wid-id-interactionsForm"
                   data-widget-colorbutton="false"
-                  data-widget-editbutton="false"
-                  data-widget-deletebutton="false"
+                  data-widget-deletebutton="true"
                   data-widget-sortable="false"
                   role="widget"
                 >
